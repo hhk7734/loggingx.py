9b52ba2f59bfd500f8371b9bcaaf94a168f0f065
Hyeonki Hong <hhk7734@gmail.com>
2023-07-21 19:32:54 +0900

    feat: add __version__ from importlib.metadata

--- loggingx/__init__.py
+++ loggingx/__init__.py
@@ -1,7 +1,14 @@
+from importlib.metadata import PackageNotFoundError, version
 from logging import *
 
 from loggingx.context import CtxRecord as _CtxRecord
 from loggingx.context import addFields
 from loggingx.formatter import JSONFormatter
+
+try:
+    __version__ = version("logginx")
+except PackageNotFoundError:
+    pass
+
 
 setLogRecordFactory(_CtxRecord)
