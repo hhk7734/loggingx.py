7b241df6085a24144253a1d4ec4898677d72e28e
Hyeonki Hong <hhk7734@gmail.com>
2024-10-23 23:48:05 +0900

    feat: add datetime parser (#8)

--- loggingx/json.py
+++ loggingx/json.py
@@ -1,4 +1,6 @@
 import json
+from datetime import datetime
+from typing import Any
 
 try:
     import orjson
@@ -7,12 +9,24 @@ try:
 except ImportError:
     orjson_imported = False
 
+
+def _default(obj: Any) -> str:
+    if isinstance(obj, datetime):
+        return obj.isoformat(sep="T")
+
+    raise TypeError(f"Object of type {type(obj)} is not JSON serializable")
+
+
+def _orjson_dumps(obj: Any) -> str:
+    return orjson.dumps(obj, default=_default).decode()
+
+
+def _json_dumps(obj: Any) -> str:
+    # Set ensure_ascii to False to output the message as it is typed.
+    return json.dumps(obj, ensure_ascii=False, default=_default)
+
+
 if orjson_imported:
-
-    def dumps(obj):
-        return orjson.dumps(obj).decode()
+    dumps = _orjson_dumps
 else:
-
-    def dumps(obj):
-        # Set ensure_ascii to False to output the message as it is typed.
-        return json.dumps(obj, ensure_ascii=False)
+    dumps = _json_dumps
