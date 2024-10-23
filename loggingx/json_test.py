90671309e637b3f6969852eb96d56971cb9b4bc9
Hyeonki Hong <hhk7734@gmail.com>
2024-10-24 00:29:37 +0900

    Revert "test2"
    This reverts commit 789a8b0223d4f2e66f1d88e4cd9b8f6099d39f8b.

---
+++ loggingx/json_test.py
@@ -0,0 +1,17 @@
+from datetime import datetime, timezone
+
+from .json import _json_dumps, _orjson_dumps
+
+dump_cases = [
+    (datetime(2024, 1, 1, 0, 0, 0, 0, tzinfo=timezone.utc), "2024-01-01T00:00:00+00:00"),
+]
+
+
+def test_json_dumps() -> None:
+    for input, expected in dump_cases:
+        assert _json_dumps({"a": input}) == f'{{"a": "{expected}"}}'
+
+
+def test_orjson_dumps() -> None:
+    for input, expected in dump_cases:
+        assert _orjson_dumps({"a": input}) == f'{{"a":"{expected}"}}'
